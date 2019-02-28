aaf334f05e2da30eb3cf9ce4e2199ab1b8240b05
Kenn <kenn+git@code-crew.org>
2019-02-28 09:37:45 -0600

    update

--- ValidationApp/views.py
+++ ValidationApp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UserForm
+from .models import UserModel
 
 
 # Create your views here.
 def index(request):
+
+    # if(request.method == "POST"):
+
+    allEntries = UserModel.objects.all()
     form = UserForm()
     context = {
-        "form": form
+        "requestMethod": request.method,
+        "allEntries": allEntries,
+        "form": form,
     }
     return render(request, "ValidationApp/index.html", context)
