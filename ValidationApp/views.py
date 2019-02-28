df1d4856db7c4d3623b8cc66adae71b25b414ebb
Kenn <kenn+git@code-crew.org>
2019-02-28 09:31:07 -0600

    update

--- ValidationApp/views.py
+++ ValidationApp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import UserForm
 
 
 # Create your views here.
 def index(request):
-
+    form = UserForm()
     context = {
         "form": form
     }
-    return render(request, "ValidationApp/index.", context)
+    return render(request, "ValidationApp/index.html", context)
